1874584b135baa953f3ba8989ff6cd77096e7863
root <root@ns4002334.ip-198-100-147.net>
2014-06-14 09:33:59 -0400

    Improved install script

--- server_side/README.md
+++ server_side/README.md
@@ -4,10 +4,10 @@ Fast and easy installation on client
 Prerequesites
 -------------
 
-This installer now only works on systemctl-based Linux distribution.
+This installer now only works on Linux distribution using 'systemctl' or 'service' to start/stop services.
 
-How to install Sbire
---------------------
+How to install Sbire (client-side)
+----------------------------------
 
 * Make sure that NRPE is up and running
 * login as root
--- server_side/install.sh
+++ server_side/install.sh
@@ -24,6 +24,12 @@ _nrpepid=$(echo ${_nrpeps} | awk '{print $2}')
 
 if [[ -z "${_nrpecfg}" ]]
 then
+	# Tenter d'installer nrpe en automatique (apt-get)
+	if  \which apt-get >/dev/null 2>&1 
+	then
+	  log "You can install NRPE with :"
+	  log "apt-get install nagios-nrpe-server"
+	fi
 	fail "Could not find NRPE config file. Is NRPE installed and running ?"
 fi
 
@@ -35,8 +41,15 @@ then
 	# Check if the PID is correct
 	_check_pid=$(systemctl status ${_nrpe_service} | grep "PID\W\+${_nrpepid}\b" | wc -l)
 	if [[ ${_check_pid} == "0" ]]; then fail "NRPE service PID does not match PID found in process (${_nrpepid})"; fi
+elif \which service >/dev/null 2>&1
+then
+	_nrpe_service=$(ls /etc/init.d | grep nrpe | awk '{print $1}')
+
+	# Check if the PID is correct
+	_check_pid=$(cat /var/run/nagios/nrpe.pid | grep "\b${_nrpepid}\b" | wc -l)
+	if [[ ${_check_pid} == "0" ]]; then fail "NRPE service PID does not match PID found in process (${_nrpepid})"; fi
 else
-	fail "Could not find how to restart service. This installer only supports 'systemctl' at this time."
+	fail "Could not find how to restart service. This installer only supports 'service' or 'systemctl' at this time."
 fi
 
 _check_sbire=$(grep sbire ${_nrpecfg} | wc -l)
@@ -65,6 +78,10 @@ if  \which systemctl >/dev/null 2>&1
 then
 	log Restarting ${_nrpe_service}...
 	systemctl restart ${_nrpe_service}
+elif  \which service >/dev/null 2>&1 
+then
+	log Restarting ${_nrpe_service}...
+	service ${_nrpe_service} restart
 fi
 
 log "SUCCESS : Sbire has been added to NRPE configuration"
